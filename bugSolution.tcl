d16f4ff628d48d884a2bdee2d5e387aabfb5aabf
BugHunter-X <askzenai@gmail.com>
2025-02-08 02:14:05 +0100

    Add bugSolution.tcl file

---
+++ bugSolution.tcl
@@ -0,0 +1,12 @@
+proc get_value {key} { 
+  if {[dict exists $my_dict $key]} { 
+    return [dict get $my_dict $key] 
+  } else { 
+    return "Key not found" 
+  } 
+}
+
+set my_dict {a 1 b 2 "a b" 3}
+puts [get_value a] ;# Output: 1
+puts [get_value d] ;# Output: Key not found
+puts [get_value "a b"] ;# Output: 3
